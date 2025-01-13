19a7dc2fc4df15a7c92ff488c20173f9196dba24
prabhjotsumman <prabhjot.nith@gmail.com>
2025-01-12 23:11:08 -0700

    producer delete route updated

--- src/routes/producerRoutes.ts
+++ src/routes/producerRoutes.ts
@@ -1,9 +1,16 @@
 import express from "express";
-import { getProducers, addProducer } from "../controllers/producerController";
+import {
+  getProducers,
+  addProducer,
+  deleteProducer,
+  updateProducer,
+} from "../controllers/producerController";
 
 const router = express.Router();
 
 router.get("/getProducers", getProducers);
 router.post("/addProducer", addProducer);
+router.put("/updateProducer/:id", updateProducer);
+router.delete("/deleteProducer/:id", deleteProducer);
 
 export default router;
